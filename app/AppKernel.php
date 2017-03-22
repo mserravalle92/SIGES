5b81db749e4de7144902f670ec63dc6a507b3e67
Mariano Serravalle <mserravalle92@gmail.com>
2017-03-22 13:44:32 -0300

    Bundles generados ApiBundle(para Services),ApplicationBundle(para la app en sí),AuthBundle(solo para controlar autentificacion y cuestiones de seguridad)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new ApiBundle\ApiBundle(),
+            new ApplicationBundle\ApplicationBundle(),
+            new AuthBundle\AuthBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/ApplicationBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ApplicationBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('ApplicationBundle:Default:index.html.twig');
+    }
+}
